230e42291316c07e9ee8f38fa5ca67ef598d1092
RoboticWater <jakebritti@gmail.com>
2020-02-14 10:04:04 -0500

    removed boilerplate + added typescript support to routing

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import { RouteComponentProps } from "@reach/router"
+
+const Home = (props: RouteComponentProps) => {
+  return (
+    <div className="Home">
+      Test
+    </div>
+  );
+}
+
+export default Home;
